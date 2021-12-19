cc6d5b96c6b85c36960041db1c9b6e77d4da6989
nekit-bes <infiless@ya.ru>
2021-12-19 16:28:27 +0300

    norminette edition

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,15 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbellero <rbellero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/19 16:20:09 by rbellero          #+#    #+#             */
+/*   Updated: 2021/12/19 16:20:09 by rbellero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_BONUS_H
-#define GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stdlib.h> //for malloc
 
-char    *get_next_line(int fd);
-char    *before_n(char  *string);
-char    *n_after(char   *string);
-char	*ft_strchr(const char   *s, int   c);
-char    *ft_strjoin(char    *s1, char  *s2);
-size_t	ft_strlen(const char *s);
-char    *null_helper(char *ex_str);
-char    *read_helper(char *buf);
+char	*get_next_line(int	fd);
+char	*before_n(char	*string);
+char	*n_after(char	*string);
+char	*ft_strchr(const char	*s, int	c);
+char	*ft_strjoin(char	*s1, char	*s2);
+size_t	ft_strlen(const char	*s);
+char	*null_helper(char	*ex_str);
+char	*read_helper(char	*buf);
 
 #endif //GET_NEXT_LINE_BONUS_H
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,71 +1,83 @@
-#include "get_next_line_bonus.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbellero <rbellero@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/19 16:20:19 by rbellero          #+#    #+#             */
+/*   Updated: 2021/12/19 16:20:19 by rbellero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-char    *null_helper(char *str)
+#include "get_next_line.h"
+
+char	*null_helper(char *str)
 {
-    if (!str)
-        return (NULL);
-    return (str);
+	if (!str)
+		return (NULL);
+	return (str);
 }
 
-char    *read_helper(char *buf)
+char	*read_helper(char *buf)
 {
-    free (buf);
-    return (NULL);
+	free (buf);
+	return (NULL);
 }
 
 size_t	ft_strlen(const	char *s)
 {
-    size_t	n;
+	size_t	n;
 
-    n = 0;
-    while (s[n])
-        n++;
-    return (n);
+	n = 0;
+	while (s[n])
+		n++;
+	return (n);
 }
 
 char	*ft_strchr(const char *s, int c)
 {
-    int		n;
-    char	j;
+	int		n;
+	char	j;
 
-    n = 0;
-    j = c;
-    if (!s)
-        return (0);
-    while (s[n])
-    {
-        if (s[n] == j)
-            return ((char *)s + n);
-        n++;
-    }
-    if (c == '\0')
-        return ((char *)s + n);
-    return (0);
+	n = 0;
+	j = c;
+	if (!s)
+		return (0);
+	while (s[n])
+	{
+		if (s[n] == j)
+			return ((char *)s + n);
+		n++;
+	}
+	if (c == '\0')
+		return ((char *)s + n);
+	return (0);
 }
 
 char	*ft_strjoin(char *s1, char *s2)
 {
-    size_t	i;
-    size_t	j;
-    char	*res;
+	size_t	i;
+	size_t	j;
+	char	*res;
 
-    if (!s1)
-    {
-        s1 = (char *)malloc(1 * sizeof(char));
-        s1[0] = '\0';
-    }
-    if (!s1 || !s2)
-        return (NULL);
-    res = malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
-    if (res == NULL)
-        return (NULL);
-    i = -1;
-    j = 0;
-    while (s1[++i] != '\0')
-        res[i] = s1[i];
-    while (s2[j] != '\0')
-        res[i++] = s2[j++];
-    res[ft_strlen(s1) + ft_strlen(s2)] = '\0';
-    free(s1);
-    return (res);
+	if (!s1)
+	{
+		s1 = (char *)malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	res = malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
+	if (res == NULL)
+		return (NULL);
+	i = -1;
+	j = 0;
+	while (s1[++i] != '\0')
+		res[i] = s1[i];
+	while (s2[j] != '\0')
+		res[i++] = s2[j++];
+	res[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (res);
 }
